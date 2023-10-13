ba7997b9ddd585cba00430f6ec8acadb3d5f77fc
Enzo D <enzo.danjour@gmail.com>
2023-10-13 11:44:11 -0300

    initial config

---
+++ src/main/java/app/mlab/todolist/user/UserModel.java
@@ -0,0 +1,27 @@
+package app.mlab.todolist.user;
+
+public class UserModel {
+    private String username;
+    private String name;
+    private String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    
+
+    
+}
---
+++ src/test/java/app/mlab/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package app.mlab.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
