ae952234561787ec77b046db4f06e89592252c60
Enzo D <enzo.danjour@gmail.com>
2023-10-16 13:02:01 -0300

    Add spring-boot-devtools dependency and create ExceptionHandlerController
    - Added the spring-boot-devtools dependency to enable hot-swapping of code changes during development.
    - Created a new class ExceptionHandlerController in the package app.mlab.todolist.errors to handle exceptions in the controller.
    - Added an exception handler method for HttpMessageNotReadableException to handle bad request errors with specific error messages.

--- src/main/java/app/mlab/todolist/task/TaskController.java
+++ src/main/java/app/mlab/todolist/task/TaskController.java
@@ -49,11 +49,22 @@ public class TaskController {
     }
 
     @PutMapping("/{id}")
-    public TaskModel update(@RequestBody TaskModel taskModel, @PathVariable UUID id, HttpServletRequest request) {
-
+    public ResponseEntity<Object> update(@RequestBody TaskModel taskModel, @PathVariable UUID id,
+            HttpServletRequest request) {
+        
         var task = this.taskRepository.findById((UUID) id).orElse(null);
+
+        if(task == null){
+             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Tarefa não encontrada");
+        }
+        var idUser = request.getAttribute("idUser");
+
+        if (!task.getIdUser().equals(idUser)) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário não tem permissões suficientes");
+        }
         Utils.copyNonNullProperties(taskModel, task);
-        return this.taskRepository.save(task);
+        var taskUpdated = this.taskRepository.save(task);
+        return ResponseEntity.ok(taskUpdated);
 
     }
 
